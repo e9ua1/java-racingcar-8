edcba1e57764b0bfe6bcc6892a23414035495ba3
dongheeHan <ark182818@gmail.com>
2025-10-25 17:43:51 +0900

    feat(view): OutputView 클래스 구현
    - printResultHeader() - 실행 결과 헤더 출력
    - printRoundResult() - 각 라운드 자동차 상태 출력
    - printWinners() - 최종 우승자 출력
    - printCarStatus() - 개별 자동차 상태 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,32 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String RESULT_HEADER = "\n실행 결과";
+    private static final String WINNER_PREFIX = "최종 우승자 : ";
+    private static final String NAME_POSITION_SEPARATOR = " : ";
+    private static final String WINNER_DELIMITER = ", ";
+
+    public void printResultHeader() {
+        System.out.println(RESULT_HEADER);
+    }
+
+    public void printRoundResult(Cars cars) {
+        for (Car car : cars.getCars()) {
+            printCarStatus(car);
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<String> winners) {
+        System.out.println(WINNER_PREFIX + String.join(WINNER_DELIMITER, winners));
+    }
+
+    private void printCarStatus(Car car) {
+        System.out.println(car.getName() + NAME_POSITION_SEPARATOR + car.getStatusBar());
+    }
+}
